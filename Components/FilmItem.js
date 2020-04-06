8ba71647a8f5fdc6d1e0f0a70f433886008c148e
AmbroiseChaigne <ambroise.chaigne@hotmail.fr>
2020-04-06 04:06:50 +0200

    Première utilisation de Redux avant la Navigation avancée

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -2,30 +2,44 @@
 
 import React from 'react'
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
-import {getImageFromApi} from '../API/TMDBApi'
+import { getImageFromApi } from '../API/TMDBApi'
 
 class FilmItem extends React.Component {
+
+  _displayFavoriteImage() {
+    if (this.props.isFilmFavorite) {
+      // Si la props isFilmFavorite vaut true, on affiche le 🖤
+      return (
+        <Image
+          style={styles.favorite_image}
+          source={require('../Images/ic_favorite.png')}
+        />
+      )
+    }
+  }
+
   render() {
     const { film, displayDetailForFilm } = this.props
     return (
-      <TouchableOpacity 
-      style={styles.main_container}
-      onPress={() => displayDetailForFilm(film.id)}>
+      <TouchableOpacity
+        style={styles.main_container}
+        onPress={() => displayDetailForFilm(film.id)}>
         <Image
-            style={styles.image}
-            source={{uri: getImageFromApi(film.poster_path)}}
+          style={styles.image}
+          source={{uri: getImageFromApi(film.poster_path)}}
         />
         <View style={styles.content_container}>
-            <View style={styles.header_container}>
-                <Text style={styles.title_text}>{film.title}</Text>
-                <Text style={styles.vote_text}>{film.vote_average}</Text>
-            </View>
-            <View style={styles.description_container}>
-                <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
-            </View>
-            <View style={styles.date_container}>
-                <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
-            </View>
+          <View style={styles.header_container}>
+            {this._displayFavoriteImage()}
+            <Text style={styles.title_text}>{film.title}</Text>
+            <Text style={styles.vote_text}>{film.vote_average}</Text>
+          </View>
+          <View style={styles.description_container}>
+            <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
+          </View>
+          <View style={styles.date_container}>
+            <Text style={styles.date_text}>Sorti le 13/12/2017</Text>
+          </View>
         </View>
       </TouchableOpacity>
     )
@@ -38,14 +52,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   image: {
-      width: 120,
-      height: 180,
-      margin: 5,
-      backgroundColor: 'gray'
+    width: 120,
+    height: 180,
+    margin: 5
   },
   content_container: {
-      flex: 1,
-      margin: 5
+    flex: 1,
+    margin: 5
   },
   header_container: {
     flex: 3,
@@ -54,29 +67,33 @@ const styles = StyleSheet.create({
   title_text: {
     fontWeight: 'bold',
     fontSize: 20,
-    flex: 5,
+    flex: 1,
     flexWrap: 'wrap',
     paddingRight: 5
   },
   vote_text: {
     fontWeight: 'bold',
     fontSize: 26,
-    flex: 1,
     color: '#666666'
   },
   description_container: {
-      flex: 7
+    flex: 7
   },
   description_text: {
-      fontStyle: 'italic',
-      color: '#666666'
+    fontStyle: 'italic',
+    color: '#666666'
   },
   date_container: {
-      flex: 1
+    flex: 1
   },
   date_text: {
-      textAlign: 'right',
-      fontSize: 14
+    textAlign: 'right',
+    fontSize: 14
+  },
+  favorite_image: {
+    width: 25,
+    height: 25,
+    marginRight: 5
   }
 })
 
